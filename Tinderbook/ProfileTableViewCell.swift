281a5a6a8189597b90c2ea05bab09b9b1215c060
ugowe <joe@spheares.com>
2016-10-13 14:45:17 -0400

    Set up profile page table view controller

--- Tinderbook/ProfileTableViewCell.swift
+++ Tinderbook/ProfileTableViewCell.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2016 Ugowe. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        profileImageView.layer.masksToBounds = true
+        profileImageView.layer.cornerRadius = 22
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+}
--- Tinderbook/TableViewController.swift
+++ Tinderbook/TableViewController.swift
@@ -10,42 +10,102 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    
+    let tinderbook = APIClient()
+    var profile: Profile? = nil
+    let token = "myToken"
+    
+    func authenticate(completion: @escaping (_ authenticated: Bool) -> ()) {
+        tinderbook.authenticateUser(facebookToken: token) { (success, token, profile) in
+            if success {
+                print("You have been authenticated! Your Tinder token: \(token)")
+                self.profile = profile
+                completion(true)
+            } else {
+                print("Unable to authenticate with Facebook token: (\(self.token))")
+                completion(false)
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func fetchRecommendations() {
+        tinderbook.getRecommendations { (success, profiles) in
+            if success {
+                if let profiles = profiles {
+                    self.viewProfiles(profiles: profiles)
+                }
+            } else {
+                print("Unable to fetch recommendations. Inspect your Tinder token")
+            }
+        }
+    }
+    
+    func viewProfiles(profiles: [Profile]) {
+        for profile in profiles {
+        print("\(profile.name) is \(profile.distance!) mi away from you.")
+        }
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return (section == 0) ? "Profile" : "Recommendations"
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 55
+    }
+    
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return (section == 0) ? 1 : 10
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        let profileCellId = "profileCell"
+        let recommendationCellId = "recommendationCell"
+        
+        if indexPath.section == 0 {
+            let profileCell = tableView.dequeueReusableCell(withIdentifier: profileCellId, for: indexPath) as! ProfileTableViewCell
+            
+            // Set image, name, details
+            
+            profileCell.profileImageView.image = UIImage(named: "empty-user")
+            profileCell.nameLabel.text = "Jesse, 30"
+            
+            return profileCell
+            
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier:recommendationCellId, for: indexPath) as! ProfileTableViewCell
+            
+            // Set image, name, age, distance
+            
+            cell.profileImageView.image = UIImage(named: "empty-user")
+            cell.nameLabel.text = "Jane, 30"
+            cell.distanceLabel?.text = "3 mi away"
+            
+            return cell
+        }
+    
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        authenticate { (authenticated) in
+            self.fetchRecommendations()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+ 
 
     /*
     // Override to support conditional editing of the table view.
